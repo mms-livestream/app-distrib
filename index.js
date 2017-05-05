deff3565af2ac67fcc50aa2a89c6ce11a94ec9cc
SaadSerhani <saad.serhani7@gmail.com>
2017-05-05 10:10:44 +0200

    2min done. Calculate CPU use.
    TODO: Send CPU use to database controller.

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var express = require("express");
 var fs = require('fs');
 let core = require("mms-core");
 let Promise = require("bluebird"); //jshint ignore:line
+//let exec = require("child_process").exec;
 
 let serverAPI = require("./api/server/module.js");
 let serviceAPI = require("./api/server/module.js");
@@ -16,6 +17,17 @@ let serviceAPI = require("./api/server/module.js");
 
 //let app = express();
 
+/*function getCPU() {
+  var childLAVG = exec ("cat /proc/loadavg | cut -d ' ' -f 1", function(errLAVG, stdoutLAVG, stderrLAVG) {
+     var childProc = exec (`grep "model name" /proc/cpuinfo | wc -l`, function(errProc, stdoutProc, stderrProc) {
+        console.log("stdout proc: " + stdoutProc);
+        console.log("stdout lavg: " + stdoutLAVG);
+	var cpuUse = 100*stdoutLAVG/stdoutProc;
+	console.log("CPU(%): " + cpuUse);
+	//TODO: SEND TO DATABASE CONTROLLER
+     });
+  });
+}*/
 
 class Distrib {
   constructor() {
@@ -23,6 +35,7 @@ class Distrib {
     this.service = new core.Service(this.node, serviceAPI);
     this.server = new core.Server(this.node, serverAPI, {
       service: this.service,
+      //toolbox: {getCPU: getCPU}
     });
     //this.server.timeout = 100000000;
   }
