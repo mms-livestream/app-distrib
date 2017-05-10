c4cabd4b28b5fe844f438042d64f84c7cb2b5957
JRillette <JRillette@users.noreply.github.com>
2017-05-10 17:05:29 +0200

    Send ipAddress and bitrate to DbController

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ let Promise = require("bluebird"); //jshint ignore:line
 let exec = require("child_process").exec;
 let request = require("request");
 let Infiniteloop = require('infinite-loop');
+let info = fs.createWriteStream('info.json'); // Json containing ipAdress and uploading bitrate
 let child1, child2, child3;
 let bitrate;
 let maxBitrate = 10;
 var tmp;
 let period = 1000; //30 secondes for the first loopBW
 let send;
+let dest = core.dConfig["NODE_DB_CONTROLLER"].service.host + ':' + core.dConfig["NODE_DB_CONTROLLER"].service.port;
 
 let serverAPI = require("./api/server/module.js");
 let serviceAPI = require("./api/server/module.js");
@@ -24,29 +26,35 @@ let serviceAPI = require("./api/server/module.js");
 let app = express();
 
 //Seek for the uploading datas to set
-child1 = exec("cat /sys/class/net/enp0s3/statistics/tx_bytes",function(error,stdout,stderr) {
+child1 = exec("cat /sys/class/net/wlo1/statistics/tx_bytes",function(error,stdout,stderr) {
   tmp = stdout;
 });
 
 //Compute the maxBitrate
 child2 = exec("speedtest-cli | grep 'Upload:' | cut -c9-13", function(errorST, stdoutST, stderrST) {
   maxBitrate = stdoutST;
+  console.log(maxBitrate);
 });
 
 //Seek for the @IP
 child3 = exec("hostname -I", function(errorIP, stdoutIP, stderrIP) {
-  send = JSON.stringify(stdoutIP).pipe(request.put('http://localhost:8081' + '/api/servers/load'));
+  info.ipAddress = stdoutIP;
+  // send = fs.createReadStream('./info.json').pipe(request.put('http://' + dest + '/api/servers/load'));
 });
 
 //Executes `cat /sys/class/net/wlo1/statistics/tx_bytes`
 function getBandwidth() {
-  var childBand = exec("cat /sys/class/net/enp0s3/statistics/tx_bytes",function(error,stdout,stderr) {
+  var childBand = exec("cat /sys/class/net/wlo1/statistics/tx_bytes",function(error,stdout,stderr) {
     bitrate = (stdout - tmp)/(period/1000);
     tmp = stdout;
-    var res = (100*8*bitrate)/(1000000*maxBitrate);
+    var res = Math.round((100*8*bitrate)/(1000000*maxBitrate));
     if( res <= 100 ) {
-	     send = JSON.stringify(bitrate/maxBitrate*10000).pipe(request.put('http://localhost:8081' + '/api/servers/load'));
+      info.bitrate = res;
+    } else {
+      info.bitrate = 100;
     }
+    console.log(info.ipAdress + ' ' + info.bitrate);
+    send = fs.createReadStream('./info.json').pipe(request.put('http://' + dest + '/api/servers/load'));
   });
 }
 
